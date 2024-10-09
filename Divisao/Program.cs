96f5e9e6cb05050fcc305575b73c0567e0270b4c
RuanMSantos <ruancauancauan@gmail.com>
2024-10-09 01:32:45 -0300

    Finaliza exercício Divisao

---
+++ Divisao/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um numerador....: ");
+int numerador = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite um denominador..: ");
+int denominador = Convert.ToInt32(Console.ReadLine());
+
+if (denominador == 0){
+    Console.WriteLine("Não é possível dividir por zero.");
+} else {
+    Console.WriteLine($"{numerador} dividido por {denominador} é {numerador / denominador}");
+}
