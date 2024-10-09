ca892c288972e8d9042298ccd69fa0577ca47eec
RuanMSantos <ruancauancauan@gmail.com>
2024-10-09 01:12:02 -0300

    Finaliza exercício Negativo

---
+++ Negativo/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite um número: ");
+int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroDigitado < 0){
+    Console.WriteLine("Número negativo");
+} else {
+    Console.WriteLine("Número positivo");
+}
