3faae42d635155c4e81eb92a02fca375cb7c5fe0
RuanMSantos <ruancauancauan@gmail.com>
2024-10-10 03:10:26 -0300

    Finaliza exercício DoadorSangue

---
+++ DoadorSangue/Program.cs
@@ -0,0 +1,8 @@
+﻿string condicao = "";
+Console.Write("Qual a sua idade? ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18 && idade <= 67) condicao = "pode";
+else condicao = "não pode";
+
+Console.WriteLine($"Você {condicao} ser doador de sangue.");
