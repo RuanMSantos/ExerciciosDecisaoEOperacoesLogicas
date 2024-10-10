1792efb408ddad402326feca49c9d8292335ca88
RuanMSantos <ruancauancauan@gmail.com>
2024-10-10 03:32:24 -0300

    Finaliza exercício ParImpar

---
+++ ParImpar/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+string designacao = "";
+if (Sobra(numero) > 0) designacao = "ímpar";
+else designacao = "par";
+
+Console.WriteLine($"{numero} é {designacao}");
+
+int Sobra(int n) => n % 2; 
