2c316df08fcceaab5ba0ed4bde87aa09892fc359
RuanMSantos <ruancauancauan@gmail.com>
2024-10-10 02:59:29 -0300

    Finaliza exercício Senha

---
+++ Senha/Program.cs
@@ -0,0 +1,9 @@
+﻿const string senha = "1234abcd";
+string autorizacao = "";
+
+Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+string senhaDigitada = Console.ReadLine()!;
+
+autorizacao = senha == senhaDigitada ? "Acesso permitido" : "Acesso negado";
+
+Console.WriteLine(autorizacao);
