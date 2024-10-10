7ff7ed9da0d057ab0457580a969df745fa4b1804
RuanMSantos <ruancauancauan@gmail.com>
2024-10-10 02:45:45 -0300

    Finaliza exercício Maior2Numeros

---
+++ Maior2Numeros/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Digite o primeiro número..: ");
+int primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Digite o segundo número...: ");
+int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+int numero = 0;
+if (primeiroNumero > segundoNumero){
+    numero = primeiroNumero;
+}
+else if (primeiroNumero == segundoNumero){
+    Console.WriteLine("Os dois números são iguais");
+    return;
+}
+else{
+    numero = segundoNumero;
+}
+Console.WriteLine($"O maior número é {numero}");
