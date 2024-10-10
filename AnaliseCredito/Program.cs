5a7d6219ba11aab9453b60e0d8cae9e717de0845
RuanMSantos <ruancauancauan@gmail.com>
2024-10-10 14:21:01 -0300

    Finaliza exercício AnaliseCredito

---
+++ AnaliseCredito/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("--- Análise crédito ---\n");
+Console.Write("Valor do empréstimo: ");
+decimal emprestimo = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Em quantas parcelas? ");
+decimal quantidadeParcela = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Renda mensal comprovada: ");
+decimal renda = Convert.ToDecimal(Console.ReadLine());
+
+decimal valorParcela = emprestimo / quantidadeParcela;
+decimal porcentagemRenda = renda / 100 * 30;
+
+if (valorParcela <= porcentagemRenda) 
+     Console.WriteLine("Autorizado.");
+else Console.WriteLine("Negado.");
