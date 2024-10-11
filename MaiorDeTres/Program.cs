6ebaa2db47709f75a7e2fd0e69993169ff18f4a2
RuanMSantos <ruancauancauan@gmail.com>
2024-10-11 01:06:47 -0300

    Finaliza exercício MaiorDeTres

---
+++ MaiorDeTres/Program.cs
@@ -0,0 +1,22 @@
+﻿double maiorNumero;
+
+Console.Write("Digite o primeiro número: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o segundo número.: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o terceiro número: ");
+double n3 = Convert.ToDouble(Console.ReadLine());
+
+if (n1 > n2 && n1 > n3){
+    maiorNumero = n1;
+}
+else if (n2 > n1 && n2 > n3){
+    maiorNumero = n2;
+}
+else {
+    maiorNumero = n3;
+}
+
+Console.WriteLine($"\nO maior número é -> {maiorNumero}");
