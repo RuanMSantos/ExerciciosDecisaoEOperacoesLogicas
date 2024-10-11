cab602e805403ed07f5b176b4ae399b1c32623db
RuanMSantos <ruancauancauan@gmail.com>
2024-10-11 01:22:27 -0300

    Finaliza exercício Multiplo

---
+++ Multiplo/Program.cs
@@ -0,0 +1,18 @@
+﻿string condicao = "";
+
+Console.WriteLine("--- Múltiplo ---\n");
+
+Console.Write("Digite um número...........: ");
+double numero =  Convert.ToDouble(Console.ReadLine());
+Console.Write("Avaliar se é múltiplo de...: ");
+double multiplicador =  Convert.ToDouble(Console.ReadLine());
+
+if (numero < 0 || multiplicador <= 0){
+    Console.WriteLine("Erro...");
+    return;
+}
+if (numero % multiplicador == 0)
+    condicao = "é";
+else condicao = "não é";
+
+Console.WriteLine($"{numero} {condicao} múltiplo de {multiplicador}");
